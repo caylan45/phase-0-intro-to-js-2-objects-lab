de5a9dcb298bdafbcc77a6f9ca467fd116c4fecc
caylan wilcox <caylan45@yahoo.com>
2022-10-22 00:14:37 -0500

    commit tree 89f17d6a2d4e29634b79e98dbe066267bef83a5c

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+   name:"John",
+   streetAddress: "bradst",
+
+};
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+    employee[key]=value;
+    return employee;
+}
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    const emply={...employee};
+    emply[key]=value;
+    return emply;
+}
+function deleteFromEmployeeByKey(employee, key)
+{
+    const empy = {...employee};
+    delete empy[key];
+    return empy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+
+delete employee[key];
+return employee;
+}
